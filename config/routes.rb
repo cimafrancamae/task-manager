c8ef04396140d3f201774d25c5dcdee26bc87a00
maeci <cimafrancamae@gmail.com>
2024-02-27 17:58:51 +0800

    Add signup, sign-in, change pwd pages style

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "tasks#index"
 
   resources :categories
   resources :tasks
+  root "tasks#index"
 end
